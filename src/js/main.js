3fb2784b587f6de77cb86d26b09ed58022ac29bb
ana.karolina <ana.karolina@ablsystem.com.br>
2022-03-16 16:35:55 -0300

    feat: configuração dos endereços dos arquivos chamados.

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ function createWindow() {
         }
     });
 
-    win.loadFile('index.html');
+    win.loadFile('src/html/index.html');
 
     ipcMain.handle('dark-mode:toggle', () => {
         if (nativeTheme.shouldUseDarkColors) {
