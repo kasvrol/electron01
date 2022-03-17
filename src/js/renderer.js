a6b34132dd1d856c98225708d35e4959035b0193
ana.karolina <ana.karolina@ablsystem.com.br>
2022-03-17 09:46:35 -0300

    feat: adição de estilo, cores e mudança no modo de ativação de tema da página.

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,15 +1,34 @@
-document
-    .getElementById('toggle-dark-mode')
-    .addEventListener('click', async () => {
+const opcaoDeTema = document.querySelector('#opcaoDeTema');
+const botaoDeConfirmacaoDeTema = document
+    .querySelector('#botaoDeConfirmacaoDeTema')
+    .addEventListener('click', escolherTema);
+
+async function escolherTema(evento) {
+    evento.preventDefault();
+
+    if (opcaoDeTema.value == 0) {
         const isDarkMode = await window.darkMode.toggle();
         document.getElementById('theme-source').innerHTML = isDarkMode
             ? 'Dark'
             : 'Light';
-    });
-
-document
-    .getElementById('reset-to-system')
-    .addEventListener('click', async () => {
+    } else if (opcaoDeTema.value == 1) {
         await window.darkMode.system();
         document.getElementById('theme-source').innerHTML = 'System';
-    });
+    }
+}
+
+// document
+//     .getElementById('toggle-dark-mode')
+//     .addEventListener('click', async () => {
+//         const isDarkMode = await window.darkMode.toggle();
+//         document.getElementById('theme-source').innerHTML = isDarkMode
+//             ? 'Dark'
+//             : 'Light';
+//     });
+
+// document
+//     .getElementById('reset-to-system')
+//     .addEventListener('click', async () => {
+//         await window.darkMode.system();
+//         document.getElementById('theme-source').innerHTML = 'System';
+//     });
